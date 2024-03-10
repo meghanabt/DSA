c7b60d3234ce50ee172826a5a2087e89dece8ed3
Meghana B T <116468110+meghanabt@users.noreply.github.com>
2024-03-10 16:34:24 -0700

    Time: 1 ms (63.03%) | Memory: 42.8 MB (34.76%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,19 +1,37 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> arr = new ArrayList<>();
-        arr.add(new ArrayList<>());
+        // List<List<Integer>> arr = new ArrayList<>();
+        // arr.add(new ArrayList<>());
 
-        for (int j : nums) {
-            List<List<Integer>> temp = new ArrayList<>();
-            for (List<Integer> i : arr) {
-                List<Integer> subset = new ArrayList<>(i);
-                System.out.println("subset"+ subset);
-                subset.add(j);
-                temp.add(subset);
+        // for (int j : nums) {
+        //     List<List<Integer>> temp = new ArrayList<>();
+        //     for (List<Integer> i : arr) {
+        //         List<Integer> subset = new ArrayList<>(i);
+        //         System.out.println("subset"+ subset);
+        //         subset.add(j);
+        //         temp.add(subset);
+        //     }
+        //     arr.addAll(temp);
+        //     System.out.println(arr);
+        // }
+        // return arr;
+
+        List<List<Integer>> sub= new ArrayList<>();
+        sub.add(new ArrayList<>());
+
+        for(int j: nums)
+        {
+            List<List<Integer>> temp= new ArrayList<>();
+            for(List<Integer> i: sub)
+            {
+                List<Integer> s= new ArrayList(i);
+                s.add(j);
+                temp.add(s);
             }
-            arr.addAll(temp);
-            System.out.println(arr);
+            sub.addAll(temp);
+            
+
         }
-        return arr;
+        return sub;
     }
 }
