6489f94fe33bbbde706ccab79d0f6b4c8690f87b
Meghana B T <116468110+meghanabt@users.noreply.github.com>
2024-06-10 13:59:53 -0700

    Time: 0 ms (100.00%) | Memory: 40.7 MB (64.84%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,26 +1,37 @@
 class Solution {
     public int rob(int[] nums) {
 
-        if(nums.length==2) return Math.max(nums[0], nums[1]);
-        int prev=nums[0];
-        int prev2= 0;
+        int[] dp= new int[nums.length+1];
+        Arrays.fill(dp, -1);
+         dp[0]=nums[0];
+         
 
-        for(int i=1;i<nums.length;i++)
-        {
-            int pick=nums[i];
+         for(int i=1; i<nums.length; i++)
+         {
+            int pick= nums[i];
+            if(i>1)
+             pick+=dp[i-2];
+            int notpick = dp[i-1];
 
-            if(i>1) pick+=prev2;
+            dp[i]=Math.max(pick, notpick);
+         }
+         return dp[nums.length-1];
+         
+      //  return checkrobbing(nums.length-1,nums, dp);
+        
+    }
 
-            int nonPick= prev;
+    public int checkrobbing(int index, int[] nums, int[] dp)
+    {
+        if(index==0) return nums[index];
+        if(index<0) return 0;
 
-            int cur_i=Math.max(pick, nonPick);
-            
-            prev2=prev;
-            prev=cur_i;
+        if(dp[index]!=-1) return dp[index];
 
-        }
+        int left= nums[index]+checkrobbing(index-2, nums, dp);
+        int right= 0+ checkrobbing(index-1, nums, dp);
 
-        return prev;
+        return dp[index]=Math.max(left, right);
         
     }
 }
